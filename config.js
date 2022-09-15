90579be803430ebd03aa54b11cf396b823fa2866
Gavin Pasqual Lifrieri <gavinlifrieri@fb.com>
2022-09-15 19:18:50 -0400

    added upload functionality

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCz282nkMqE32DuK87bNivzCWnZO3Y-Cjo",
+    authDomain: "yuck-aa45d.firebaseapp.com",
+    projectId: "yuck-aa45d",
+    storageBucket: "yuck-aa45d.appspot.com",
+    messagingSenderId: "685620015428",
+    appId: "1:685620015428:web:aed368ecf36b5f32ece711",
+    measurementId: "G-XEHKQYJM2M"
+  };
+
+if (!firebase.apps.length){
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+}
+
+export {firebase}
