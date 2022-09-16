aebbffca09d2ecc582ff975cfc600fefc0b2919f
Gavin Pasqual Lifrieri <gavinlifrieri@fb.com>
2022-09-15 20:51:53 -0400

    added joystick

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Joystick from './src/Joystick';
 import UploadScreen from './src/UploadScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <UploadScreen/>
+      <Joystick/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#B6F9C9',
     flex: 1,
   },
 });
